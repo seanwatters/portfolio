35ed5a70bd3e753df60da773c103e1a82f2bd119
sean watters <sean@watters.io>
2020-09-13 15:53:22 -0700

    stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,14 @@ import useScroll from 'hooks/useScroll';
 
 import Layout from 'components/Layout';
 
+const TITLE_WRAPPER_HEIGHT = 450;
+const SECTION_HEIGHT = 900;
+
+const SECTION_ONE_BREAK = TITLE_WRAPPER_HEIGHT / 2;
+const SECTION_TWO_BREAK = TITLE_WRAPPER_HEIGHT + (SECTION_HEIGHT / 2);
+const SECTION_THREE_BREAK = TITLE_WRAPPER_HEIGHT + SECTION_HEIGHT + (SECTION_HEIGHT / 2);
+const SECTION_FOUR_BREAK = TITLE_WRAPPER_HEIGHT + (SECTION_HEIGHT * 2) + (SECTION_HEIGHT / 2);
+
 const Home = () => {
   const { scrollY } = useScroll();
 
@@ -10,11 +18,19 @@ const Home = () => {
   const [sectionTwoVisible, setSectionTwoVisible] = useState(false);
   const [sectionThreeVisible, setSectionThreeVisible] = useState(false);
   const [sectionFourVisible, setSectionFourVisible] = useState(false);
-  const [sectionFiveVisible, setSectionFiveVisible] = useState(false);
 
   useEffect(() => {
     switch (true) {
-      case scrollY >= 225:
+      case scrollY >= SECTION_FOUR_BREAK:
+        setSectionFourVisible(true);
+        break;
+      case scrollY >= SECTION_THREE_BREAK:
+        setSectionThreeVisible(true);
+        break;
+      case scrollY >= SECTION_TWO_BREAK:
+        setSectionTwoVisible(true);
+        break;
+      case scrollY >= SECTION_ONE_BREAK:
         setSectionOneVisible(true);
         break;
       default:
@@ -25,47 +41,65 @@ const Home = () => {
   return (
     <>
       <Layout title="Sean Watters — Software Engineer">
-        <h1 className="header-title">Developer. Designer. Software&nbsp;Engineer.</h1>
-        <section className="section-one">
+        <div className="page-title-wrapper">
+          <h1 className="page-title-1">Developer.</h1>
+          <h1 className="page-title-2">Designer.</h1>
+          <h1 className="page-title-3">Engineer.</h1>
+        </div>
+        <section className="section-1">
           <img
-            className={`section-one-browser-graphic ${sectionOneVisible ? 'visible' : 'hidden'}`}
+            className={`section-1-browser-graphic ${sectionOneVisible ? 'visible' : 'hidden'}`}
             src="images/web-frameworks-browser.svg"
             alt="Web Development Tools"
           />
           <img
-            className={`section-one-vertical-mobile-graphic ${sectionOneVisible ? 'visible' : 'hidden'}`}
+            className={`section-1-vertical-mobile-graphic ${sectionOneVisible ? 'visible' : 'hidden'}`}
             src="images/mobile-frameworks-vertical.svg"
             alt="Mobile Development Tools"
           />
           <img
-            className={`section-one-horizontal-mobile-graphic ${sectionOneVisible ? 'visible' : 'hidden'}`}
+            className={`section-1-horizontal-mobile-graphic ${sectionOneVisible ? 'visible' : 'hidden'}`}
             src="images/mobile-frameworks-horizontal.svg"
             alt="Mobile Development Tools"
           />
         </section>
-        <section className="section-two">
-          <h1 className="section-header">Web Development</h1>
-          <p>
-            This web page is built with React.js — more specifically it&apos;s a
-            statically rendered React application built using Next.js and hosted on a Netlify CDN.
-          </p>
-          <p>
-            You see this same website built with <a href="https://vue.seanwatters.io">Vue.js</a> and <a href="https://svelte.seanwatters.io">Svelte</a>.
-          </p>
-        </section>
-        <section className="section-three">
-          <div>
-            <h1 className="section-header">Mobile Development</h1>
+        <section className="section-2">
+          <h1 className={`section-header ${sectionTwoVisible ? 'visible' : 'hidden'}`}>Web Development</h1>
+          <div className={`section-body ${sectionTwoVisible ? 'visible' : 'hidden'}`}>
             <p>
-              statically rendered React application built using Next.js and hosted on a Netlify CDN.
               This web page is built with React.js — more specifically it&apos;s a
+              statically rendered React application built using Next.js and hosted on a Netlify CDN.
+            </p>
+            <p>
+              You see this same website built with <a href="https://vue.seanwatters.io">Vue.js</a> and <a href="https://svelte.seanwatters.io">Svelte</a>.
+            </p>
+          </div>
+        </section>
+        <section className="section-3">
+          <div className="pin-content-right">
+            <h1 className={`section-header ${sectionThreeVisible ? 'visible' : 'hidden'}`}>Mobile Development</h1>
+            <div className={`section-body ${sectionThreeVisible ? 'visible' : 'hidden'}`}>
+              <p>
+                statically rendered React application built using Next.js and hosted
+                on a Netlify CDN. This web page is built with
+                React.js — more specifically it&apos;s a
+                You see this same website built with.
+                ion built using Next.js and hosted on a Netlify CDN.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="section-4">
+          <h1 className={`section-header ${sectionFourVisible ? 'visible' : 'hidden'}`}>Designer</h1>
+          <div className={`section-body ${sectionFourVisible ? 'visible' : 'hidden'}`}>
+            <p>
+              statically rendered React application built using Next.js and hosted
+              on a Netlify CDN. This web page is built with
+              React.js — more specifically it&apos;s a
               You see this same website built with.
               ion built using Next.js and hosted on a Netlify CDN.
             </p>
           </div>
-        </section>
-        <section className="section-four">
-          <h1 className="section-header">Design</h1>
         </section>
       </Layout>
 
@@ -74,55 +108,71 @@ const Home = () => {
           width: 580px;
           margin-bottom: 20px;
         }
-      
-        .header-title {
-          margin-top: 150px;
+        .page-title-wrapper {
+          height: ${TITLE_WRAPPER_HEIGHT}px;
           margin-left: 20vw;
-          font-size: 4.5rem;
-          width: 0;
-          margin-bottom: 175px;
+          display: flex;
+          justify-content: center;
+          flex-direction: column;
+        }
+        .page-title-1,
+        .page-title-2,
+        .page-title-3 {
+          font-size: 5rem;
+          opacity: 0;
           animation: titleFadeIn;
           animation-duration: 2s;
+          animation-fill-mode: forwards;  
+        }
+        .page-title-2 {
+          animation-delay: 0.5s;
+        }
+        .page-title-3 {
+          animation-delay: 1s;
         }
         @keyframes titleFadeIn {
           to {
             opacity: 1;
           }
-          from {
-            opacity: 0;
-          }
         }
 
-
-        .section-one {
+        section {
+          height: ${SECTION_HEIGHT}px;
+          margin: 0 auto;
+          width: 60vw;
           position: relative;
-          height: 1000px;
+        }
+        
+        .section-1 {
+          width: 100vw;
         }
 
-        .visible.section-one-browser-graphic {
+        .visible.section-1-browser-graphic {
           width: 60vw;
           top: 0;
           left: 15vw;
           transition: top 1.25s, opacity 1.25s;
         }
 
-        .visible.section-one-vertical-mobile-graphic {
+        .visible.section-1-vertical-mobile-graphic {
           width: 22.5vw;
           top: 10vw;
           right: 16vw;
           transition: top 1.25s, opacity 1.5s;
+          transition-delay: 0.25s;
         }
 
-        .visible.section-one-horizontal-mobile-graphic {
+        .visible.section-1-horizontal-mobile-graphic {
           height: 22.5vw;
           top: 25vw;
           right: 22.5vw;
           transition: top 1.25s, opacity 1.75s;
+          transition-delay: 0.5s;
         }
 
-        .section-one-browser-graphic,
-        .section-one-vertical-mobile-graphic,
-        .section-one-horizontal-mobile-graphic  {
+        .section-1-browser-graphic,
+        .section-1-vertical-mobile-graphic,
+        .section-1-horizontal-mobile-graphic  {
           position: absolute;
         }
 
@@ -130,33 +180,38 @@ const Home = () => {
           opacity: 1;
         }
 
-        .hidden.section-one-browser-graphic,
-        .hidden.section-one-vertical-mobile-graphic,
-        .hidden.section-one-horizontal-mobile-graphic  {
+        .hidden.section-1-browser-graphic,
+        .hidden.section-1-vertical-mobile-graphic,
+        .hidden.section-1-horizontal-mobile-graphic  {
           top: 100vh;
           opacity: 0;
         }
+
+        .section-header {
+          transition: opacity 1s;
+        }
+        .section-body {
+          transition: opacity 2s;
+          transition-delay: 0.75s;
+        }
+        .visible.section-header,
+        .visible.section-body {
+          opacity: 1;
+        }
+        .hidden.section-header,
+        .hidden.section-body {
+          opacity: 0;
+        }
         
-        .section-two {
-          width: 60vw;
-          margin: 0 auto;
-          height: 80vh;
-          margin-top: 70px;
-          position: relative;
+        .section-2 {
         }
 
-        .section-three {
-          width: 60vw;
-          margin: 0 auto;
-          height: 70vh;
+        .section-3 {
           display: flex;
           justify-content: flex-end;
         }
 
-        .section-four {
-          width: 60vw;
-          margin: 0 auto;
-          height: 70vh;
+        .section-4 {
         }
     `}
       </style>
