5a2bd77367810b8c8d5359f455ed878fe01b9466
sean watters <sean@watters.io>
2020-09-20 13:48:24 -0700

    animations added

--- sapper/src/routes/index.svelte
+++ sapper/src/routes/index.svelte
@@ -1,19 +1,43 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import Layout from '../components/Layout.svelte';
   import SectionOne from '../components/HomePage/SectionOne.svelte';
   import SectionTwo from '../components/HomePage/SectionTwo.svelte';
   import SectionThree from '../components/HomePage/SectionThree.svelte';
 
-  $: sectionOneVisible = true;
-  $: sectionTwoVisible = true;
-  $: sectionThreeVisible = true;
+  let sectionOne: any;
+  let sectionTwo: any;
+  let sectionThree: any;
+
+  let sectionOneVisible = false;
+  let sectionTwoVisible = false;
+  let sectionThreeVisible = false;
+
+  onMount(() => {
+    setTimeout(() => {
+      sectionOneVisible = true;
+    }, 1);
+  });
+
+  const handleScroll = () => {
+    if (sectionTwo.getBoundingClientRect().top < 500) sectionTwoVisible = true;
+    if (sectionThree.getBoundingClientRect().top < 500)
+      sectionThreeVisible = true;
+  };
 </script>
 
-<!-- <svelte:window bind:scrollY={y}/> -->
+<svelte:window on:scroll="{handleScroll}" />
+
 <div class="container">
   <Layout theme="dark">
-    <SectionOne isVisible="{sectionOneVisible}" />
-    <SectionTwo isVisible="{sectionTwoVisible}" />
-    <SectionThree isVisible="{sectionThreeVisible}" />
+    <div bind:this="{sectionOne}">
+      <SectionOne isVisible="{sectionOneVisible}" />
+    </div>
+    <div bind:this="{sectionTwo}">
+      <SectionTwo isVisible="{sectionTwoVisible}" />
+    </div>
+    <div bind:this="{sectionThree}">
+      <SectionThree isVisible="{sectionThreeVisible}" />
+    </div>
   </Layout>
 </div>
