09b04d116d7f8d87831a8718db7af30724cf63e3
sean watters <sean@watters.io>
2020-09-20 15:31:44 -0700

    added analytics

--- sapper/src/components/Layout.svelte
+++ sapper/src/components/Layout.svelte
@@ -12,6 +12,19 @@
     crossorigin="anonymous"
     async>
   </script>
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=UA-167813886-1">
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-167813886-1');
+  </script>
 </svelte:head>
 
 <Header location="{location}" />
